4fe5e087c61009d33b6c0413b43c5e0a5a5ae6e1
mzwa1080 <mshawnlouw@gmail.com>
2024-08-15 11:05:10 +0200

    I checked if the item is in the cart and then update the isInCart and subscribe to it

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Book } from '../interface/Book';
 import { CartService } from '../cart/cart.service';
 import { BehaviorService } from '../shared/behavior.service';
@@ -8,43 +8,50 @@ import { BehaviorService } from '../shared/behavior.service';
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
-export class BookComponent implements OnChanges {
-  constructor(private behaviorService: BehaviorService){
+export class BookComponent implements OnInit {
+  constructor(private behaviorService: BehaviorService) {
 
   }
-  isInCart:boolean = false
+  isInCart: boolean = false
+  quantity = 0;
 
 
   @Input() book: Book = {} as Book;
 
-  ngOnChanges(changes : SimpleChanges ) : void{
-    // console.log(changes);
-    for(const bookDetails in changes){
-      // let bboook = JSON.stringify(this.book)
-      console.log(bookDetails);
-      
-      
-    }
-    
+  ngOnInit(): void {
+    this.checkIfInCart()
   }
 
-  addToCart(){
+  // Need to check if item that is seleccted from the behavior is in the cart.
+  checkIfInCart() {
+    this.behaviorService.getItems().subscribe({
+      next:(cartItems : Book[] ) =>{
+        const ItemsInCart = cartItems.find(item => item.id === this.book.id)
+        if(ItemsInCart){
+          this.isInCart = true;
+          this.quantity = ItemsInCart.quantity
+        }
+      }
+  })
+  }
+
+  addToCart() {
     this.isInCart = true;
     this.behaviorService.add(this.book)
-    
+
   }
 
   // @Output() emitBook = new EventEmitter<Book>()
 
 
-  removeFromCart(){
-  this.isInCart = false
-  this.behaviorService.subtract(this.book)    
+  removeFromCart() {
+    this.isInCart = false
+    this.behaviorService.subtract(this.book)
   }
 
-// You need method to add to cart shart when clicking rthe + butnno
+  // You need method to add to cart shart when clicking rthe + butnno
 
-// method to calculate the lenghrt the inisde the moethid , then its the one I'm going interpolate
+  // method to calculate the lenghrt the inisde the moethid , then its the one I'm going interpolate
 
 
 }
